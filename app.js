65039d5f630debfc218f93bf9f5af0f92bcf05fa
Ayon <ayonjodder177@gmail.com>
2022-02-13 15:17:14 +0600

    added .gitignore

--- app.js
+++ app.js
@@ -4,34 +4,34 @@ function generatePin() {
   const randomNum = Math.round(Math.random() * 1000000) + "";
   if (randomNum.length == 6) {
     const generateScreen = document.getElementById("generate-screen");
-      generateScreen.value = randomNum;
+    generateScreen.value = randomNum;
   } else {
-      generatePin();
-    }
-};
+    generatePin();
+  }
+}
 //Handle keypad
 document.getElementById("calc-body").addEventListener("click", (e) => {
-    const number = e.target.innerText;
-    let inputNum = document.getElementById('input-num');
-    if (isNaN(number)) {
-        if (number == 'C') {
-            inputNum.value = '';
-        }
-    } else {
-        inputNum.value = inputNum.value + e.target.innerText;
+  const number = e.target.innerText;
+  let inputNum = document.getElementById("input-num");
+  if (isNaN(number)) {
+    if (number == "C") {
+      inputNum.value = "";
     }
+  } else {
+    inputNum.value = inputNum.value + e.target.innerText;
+  }
 });
 //Compare pin
 function submitBtn() {
-    const generatePin = document.getElementById('generate-screen').value;
-    const inputPin = document.getElementById('input-num').value;
-    const notifySuccess = document.getElementById('notify-success');
-    const notifyFailed = document.getElementById('notify-failed');
-    if (generatePin == inputPin) {
-        notifySuccess.style.display = 'block';
-        notifyFailed.style.display = 'none';
-    } else {
-        notifySuccess.style.display = 'none';
-        notifyFailed.style.display = 'block';
-    }
-};
+  const generatePin = document.getElementById("generate-screen").value;
+  const inputPin = document.getElementById("input-num").value;
+  const notifySuccess = document.getElementById("notify-success");
+  const notifyFailed = document.getElementById("notify-failed");
+  if (generatePin == inputPin) {
+    notifySuccess.style.display = "block";
+    notifyFailed.style.display = "none";
+  } else {
+    notifySuccess.style.display = "none";
+    notifyFailed.style.display = "block";
+  }
+}
